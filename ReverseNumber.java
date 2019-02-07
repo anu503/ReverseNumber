6f3977e848e5da0f0611818bc1e7946a2ee908df
anudanda503 <chetandanda@gmail.com>
2019-02-07 12:17:18 -0600

    modified

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class ReverseNumber {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the number:");
-		int num=sc.nextInt();
+		//Scanner sc=new Scanner(System.in);
+		//System.out.println("enter the number:");
+		//int num=sc.nextInt();
+		int num=1234;
 		int rev=0;
 		while(num!=0)
 		{   
